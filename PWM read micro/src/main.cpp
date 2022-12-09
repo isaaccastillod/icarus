e24a7fc1d3c4e02cae56662df451ffcb7fc80968
Cedric Murphy <cedricmurphy0@gmail.com>
2022-12-08 22:44:58 -0800

    added yaw parameters

--- PWM read micro/src/main.cpp
+++ PWM read micro/src/main.cpp
@@ -28,7 +28,7 @@ ESC myESC1 (FRONT_RIGHT, SPEED_MIN, SPEED_MAX, 500);                 // ESC_Name
 ESC myESC2 (FRONT_LEFT, SPEED_MIN, SPEED_MAX, 500);                 // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
 ESC myESC3 (BACK_LEFT, SPEED_MIN, SPEED_MAX, 500);                 // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
 ESC myESC4 (BACK_RIGHT, SPEED_MIN, SPEED_MAX, 500);                 // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
-int oESC;                                                 // Variable for the speed sent to the ESC
+bool calibrated = false;
 
 const int groundpin = A4;             // analog input pin 4 -- ground
 const int powerpin = A5;              // analog input pin 5 -- voltage
@@ -37,8 +37,8 @@ const int ypin = A2;                  // y-axis
 const int zpin = A1;                  // z-axis (only on 3-axis models)
 int throttle_speed = SPEED_MIN;
 int desired_yaw = 1500;
-int desired_y = 1500;
-int desired_x = 1500;
+int desired_pitch = 1500;
+int desired_roll = 1500;
 int rs0 = 1500;
 int rs1 = 1500;
 
@@ -75,18 +75,23 @@ struct PID_
 
 PID_ pitchPID;
 PID_ yawPID;
+PID_ rollPID;
 
 Angles eulerAng;
 
 void initPIDs() {
-  pitchPID.kD = 0.3;
-  pitchPID.kP = 0.5;
-  pitchPID.kI = 0;
+  pitchPID.kD = 1.3;
+  pitchPID.kP = 0.04;
+  pitchPID.kI = 18; //0
   pitchPID.lastDerivative = 0;
-  yawPID.kD = 0.3;
-  yawPID.kP = 0.5;
-  yawPID.kI = 0;
-  yawPID.lastDerivative = 0;
+  rollPID.kD = pitchPID.kD; //oscillation
+  rollPID.kP = pitchPID.kP; //magnitude of difference between motors
+  rollPID.kI = pitchPID.kI ; //0
+  rollPID.lastDerivative = 0;
+  yawPID.kD = 4; //oscillation
+  yawPID.kP = 0.02; //magnitude of difference between motors
+  yawPID.kI = 0 ; //0
+  yawPID.lastDerivative = 0;  
 }
 
 double pd(double desiredAngle, double currAngle, double dt, PID_ pid) {
@@ -102,23 +107,21 @@ void read_gyro(sensors_event_t sensor) {
   eulerAng.x = sensor.gyro.x;
   eulerAng.y = sensor.gyro.y;
   eulerAng.z = sensor.gyro.z;
-  // Serial.print("\tx= ");
-  // Serial.print(eulerAng.x);
-  // Serial.print(" |\ty= ");
-  // Serial.print(eulerAng.y);
-  // Serial.print(" |\tz= ");
-  // Serial.println(eulerAng.z);
+  Serial.print("\tx= ");
+  Serial.print(eulerAng.x);
+  Serial.print(" |\ty= ");
+  Serial.print(eulerAng.y);
+  Serial.print(" |\tz= ");
+  Serial.println(eulerAng.z);
 }
 /* Set the delay between fresh samples */
-uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 1; //originally 100
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 //                                   id, address
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 void ESC_startup() {
-  pinMode(LED_PIN, OUTPUT);                               // LED Visual Output  
-  Serial.println("5 seconds to plug in battery");
-  delay(4000);
+  pinMode(LED_PIN, OUTPUT);                              // LED Visual Output    
   Serial.println("3 second to plug in battery");
   delay(3000);
   Serial.println("Arming now");
@@ -139,6 +142,13 @@ void ESC_startup() {
   myESC4.speed(SPEED_MIN); 
   delay(2000); 
   Serial.println("Drone armed");
+}
+
+void custom_calibration() {
+  level_x = eulerAng.x;
+  level_y = eulerAng.y;
+  level_z = eulerAng.z;
+  Serial.println("ground calibrated");
 }
 
 void setup(void)
@@ -176,29 +186,73 @@ void loop(void)
   desired_yaw = pulseIn(INPUT_YAW, HIGH);
   rs0 = pulseIn(INPUT_RS0, HIGH);
   rs1 = pulseIn(INPUT_RS1, HIGH);
-  desired_y = rs0 + (1500 - rs1);
-  desired_x = rs1 - (1500 - rs0);
+  desired_pitch = rs0 + (1500 - rs1);
+  desired_roll = rs1 - (1500 - rs0);
+  
   // Serial.println(throttle_speed);  
+
+  
+  //could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
+  sensors_event_t orientationData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);  
+
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  // Serial.println();
+  // Serial.print("Calibration: Sys=");
+  // Serial.print(system);
+  
+  // Serial.print(" Orient=");
+  // Serial.print("Orient:");
+  read_gyro(orientationData);
+
+  curr = millis();
+  dt = curr - prev_t;
+  prev_t = curr;
+
+  if (!calibrated) {
+    custom_calibration();
+    calibrated = true;
+  }
+
+  // L_offset = -pd(0, eulerAng.y, dt, pitchPID);
+  // R_offset = pd(0, eulerAng.y, dt, pitchPID);
+
+  // Serial.print(" |\teulerAng.y= ");
+  // Serial.print(eulerAng.y);
+  // Serial.print(" |\teulerAng.z= ");
+  // Serial.print(eulerAng.z);
+
+  // Four motor controller
+  // (currently ignoring roll for simplicity)
+  W1_offset = pd(level_y, eulerAng.y, dt, pitchPID)+pd(level_z, eulerAng.z, dt, rollPID)+pd(level_x, eulerAng.x, dt, rollPID); //front left
+  W2_offset = pd(level_y, eulerAng.y, dt, pitchPID)-pd(level_z, eulerAng.z, dt, rollPID)-pd(level_x, eulerAng.x, dt, rollPID); //front right
+  W3_offset = -pd(level_y, eulerAng.y, dt, pitchPID) + pd(level_z, eulerAng.z, dt, rollPID)-pd(level_x, eulerAng.x, dt, rollPID); //back left
+  W4_offset = -pd(level_y, eulerAng.y, dt, pitchPID) - pd(level_z, eulerAng.z, dt, rollPID)+pd(level_x, eulerAng.x, dt, rollPID);; //back right
+
+    // esc_1 = throttle - pid_output_pitch + pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 1 (front-right - CCW)
+    // esc_2 = throttle + pid_output_pitch + pid_output_roll + pid_output_yaw; //Calculate the pulse for esc 2 (rear-right - CW)
+    // esc_3 = throttle + pid_output_pitch - pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 3 (rear-left - CCW)
+    // esc_4 = throttle - pid_output_pitch - pid_output_roll + pid_output_yaw; //Calculate the pulse for esc 4 (front-left - CW)    
+
+  Serial.print(" |\tW1_offset= ");
+  Serial.print(W1_offset);
+  Serial.print(" |\tW2_offset= ");
+  Serial.print(W2_offset);
+  Serial.print(" |\tW3_offset= ");
+  Serial.print(W3_offset);
+  Serial.print(" |\tW4_offset= ");
+  Serial.print(W4_offset);
 
   int tot_W1 = throttle_speed + W2_offset; //front left
   int tot_W2 = throttle_speed + W1_offset; //front right
   int tot_W3 = throttle_speed + W3_offset; //back left
   int tot_W4 = throttle_speed + W4_offset; //back right
 
-  // int tot_W1 = throttle_speed; //front left
-  // int tot_W2 = throttle_speed; //front right
-  // int tot_W3 = throttle_speed; //back left
-  // int tot_W4 = throttle_speed; //back right
-
   int tot_FR_speed = tot_W2;
   int tot_FL_speed = tot_W1;
   int tot_BR_speed = tot_W4;
-  int tot_BL_speed = tot_W3;
-
-  // int tot_FR_speed = throttle_speed;
-  // int tot_FL_speed = throttle_speed;
-  // int tot_BR_speed = throttle_speed;
-  // int tot_BL_speed = throttle_speed;
+  int tot_BL_speed = tot_W3; 
 
   //DO NOT REMOVE, SAFETY
   if (tot_FR_speed > SPEED_MID || tot_FL_speed > SPEED_MID || tot_BL_speed > SPEED_MID || tot_BR_speed > SPEED_MID) {
@@ -220,62 +274,6 @@ void loop(void)
   // Serial.print(tot_BR_speed);
   // Serial.print(" |\ttot_BL_speed= ");
   // Serial.println(tot_BL_speed);
-  //could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
-  sensors_event_t orientationData;
-  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);  
 
-  uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-  // Serial.println();
-  // Serial.print("Calibration: Sys=");
-  // Serial.print(system);
-  
-  // Serial.print(" Orient=");
-  // Serial.print("Orient:");
-  read_gyro(orientationData);
-
-  curr = millis();
-  dt = curr - prev_t;
-  prev_t = curr;
-
-  // L_offset = -pd(0, eulerAng.y, dt, pitchPID);
-  // R_offset = pd(0, eulerAng.y, dt, pitchPID);
-
-  Serial.print(" |\teulerAng.y= ");
-  Serial.print(eulerAng.y);
-  Serial.print(" |\teulerAng.z= ");
-  Serial.print(eulerAng.z);
-
-  if (curr - init_t > 3000) {
-  // Four motor controller
-  // (currently ignoring roll for simplicity)
-    W1_offset = pd(0, eulerAng.y, dt, pitchPID) + pd(0, eulerAng.z, dt, yawPID); //front left
-    W2_offset = pd(0, eulerAng.y, dt, pitchPID) - pd(0, eulerAng.z, dt, yawPID); //front right
-    W3_offset = -pd(0, eulerAng.y, dt, pitchPID) + pd(0, eulerAng.z, dt, yawPID); //back left
-    W4_offset = -pd(0, eulerAng.y, dt, pitchPID) - pd(0, eulerAng.z, dt, yawPID); //back right
-  }
-  // if (R_offset < -200) {
-  //   R_offset = -200;
-  // }
-  // if (L_offset < -200) {
-  //   L_offset = -200;
-  // }
-  // if (R_offset > 200) {
-  //   R_offset = 200;
-  // }
-  // if (L_offset > 200) {
-  //   L_offset = 200;
-  // }
-
-  // Serial.print(" |\tW1_offset= ");
-  // Serial.print(W1_offset);
-  // Serial.print(" |\tW2_offset= ");
-  // Serial.print(W2_offset);
-  // Serial.print(" |\tW3_offset= ");
-  // Serial.print(W3_offset);
-  // Serial.print(" |\tW4_offset= ");
-  // Serial.print(W4_offset);
-
-  Serial.println("--");
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
